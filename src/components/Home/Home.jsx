2dab22246d601cb075ea3c62894cb8eea7d29824
Suraj <kumarsuraj54057@gmail.com>
2024-03-26 23:27:12 +0530

    initial commit

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,75 +1,68 @@
-import React from 'react';
-import './Home.css';
-import Search from '../Layout/Search/Search';
-import Featured from '../Featured/Featured';
-import Testnomial from '../Layout/Testnomials/Testnomial';
-import Work from '../Work/Work';
-import SomeDetails from '../Work/SomeDetails';
-import Banner from '../Layout/Banner/Banner';
+import React from "react";
+import "./Home.css";
+import Search from "../Layout/Search/Search";
+import Featured from "../Featured/Featured";
+import Testnomial from "../Layout/Testnomials/Testnomial";
+import Work from "../Work/Work";
+import SomeDetails from "../Work/SomeDetails";
+import Banner from "../Layout/Banner/Banner";
 
 const Home = () => {
   return (
-  <>
-    <div className='main-container'>
-      <Search />
+    <>
+      <div className="main-container">
+        <Search />
 
-      <div className="heading">
-        <h1>Most Popular Round-trip Flight Destinations</h1>
+        <div className="heading">
+          <h1>Most Popular Round-trip Flight Destinations</h1>
+        </div>
+        <div className="optionContainer">
+          <div className="btn">
+            <button>Europe</button>
+          </div>
+
+          <div className="btn">
+            <button>South America</button>
+          </div>
+
+          <div className="btn">
+            <button>South Asia</button>
+          </div>
+
+          <div className="btn">
+            <button>Middle East</button>
+          </div>
+
+          <div className="btn">
+            <button>Africa</button>
+          </div>
+
+          <div className="btn">
+            <button>Asia & Oceania</button>
+          </div>
+        </div>
+        <div className="homeContainer">
+          <Featured />
+        </div>
       </div>
-      <div className="optionContainer">
 
+      <section>
+        <Testnomial />
+      </section>
 
-        <div className="btn">
-          <button>Europe</button>
-        </div>
+      <section>
+        <Work />
+      </section>
 
-        <div className="btn">
-          <button>South America</button>
-        </div>
+      <section>
+        <SomeDetails />
+      </section>
 
-        <div className="btn">
-          <button>South Asia</button>
-        </div>
-
-        <div className="btn">
-          <button>Middle East</button>
-        </div>
-
-        <div className="btn">
-          <button>Africa</button>
-        </div>
-
-        <div className="btn">
-          <button>Asia & Oceania</button>
-        </div>
-      </div>
-      <div className="homeContainer">
-        <Featured />
-      </div>
-    </div>
-
-    <section>
-
-      <Testnomial/>
-    </section>
-
-<section>
-  <Work/>
-</section>
-
-<section>
-  <SomeDetails/>
-</section>
-
-
-<section>
-  <Banner/>
-</section>
-  </>
-
-  
-
+      <section>
+        <Banner />
+      </section>
+    </>
   );
-}
+};
 
 export default Home;
